2189dcdd08edda8748db0c8dad1ff2cc883f77c8
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2023-12-26 17:58:29 +0000

    conversion

--- sub/conversion.js
+++ sub/conversion.js
@@ -1,6 +1,6 @@
 let score = "33abc"
 
-/*console.log(typeof score );
+console.log(typeof score );
 console.log(typeof (score) );
 
 let valueInNumber = Number(score)
@@ -27,7 +27,7 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
-*/ 
+ 
 
 //OPERATION
 
