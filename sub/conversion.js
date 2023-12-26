00337b66dd2c8d1eb19ec715110bb8bf7a54b290
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2023-12-26 17:57:12 +0000

    conversion

--- sub/conversion.js
+++ sub/conversion.js
@@ -1,6 +1,6 @@
 let score = "33abc"
 
-console.log(typeof score );
+/*console.log(typeof score );
 console.log(typeof (score) );
 
 let valueInNumber = Number(score)
@@ -27,3 +27,37 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/ 
+
+//OPERATION
+
+let value = 4
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Ritesh"
+
+let str3 = str1 + str2
+console.log(typeof str3);
+console.log( str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
