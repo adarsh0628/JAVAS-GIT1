0e578f7030f180127016017c538c27843f0cb6fd
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2023-12-26 16:29:08 +0000

    datatype

---
+++ sub/dataType.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as never version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log();
+
+let name = "Ritesh"
+
+let age =  18
+
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+console.log(typeof age); //number
+
+console.log(typeof "Ritesh"); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
