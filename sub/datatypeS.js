48efe6c3995815a2b31071a4ece7bceae8ff055b
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2023-12-28 16:02:44 +0000

    datas

---
+++ sub/datatypeS.js
@@ -0,0 +1,26 @@
+ // Premitive
+
+ // 7 types => String, Number, boolean, null, undefined, symbol, BigInt
+
+ const id = Symbol('123')
+ const anotherid = Symbol('123')
+console.log(id == anotherid);
+
+
+  const bigNumber = 33434548686878827837n
+
+
+
+ // Reference(Non-premitive)
+
+ // Array, Objects, Functions
+
+ const heros = ["Ritesh", "Nitish", "Adarsh"]
+let myobj = {
+    name: "Ritesh"
+
+ }
+
+ const myfunction = function(){
+    console.log("Hello World");
+ }
