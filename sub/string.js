39dc510dfc30811aa010c110e4281f836b1982bc
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2024-01-04 08:56:44 +0000

    string

---
+++ sub/string.js
@@ -0,0 +1,23 @@
+const MyName = new String('Riteshoc')
+// console.log(MyName[0]);
+// console.log(MyName.length);
+// console.log(MyName.toUpperCase());
+// console.log(MyName.charAt(4));
+// console.log(MyName.indexOf('h'));
+
+const MyName1 = "Ritesh"
+const newstring = MyName1.slice(0,4)
+console.log(newstring);
+
+const newstringone ="  RITESH1  "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+
+const url = "https://Ritesh.com/Ritesh%20Singh"
+console.log(url.replace('%20','_'));
+console.log(url.includes('Ritesh'));
+
+
+const one = new String('Ritesh-oc-god')
+console.log(one.split('-'));
