1a907a6ad9d2c6fb3211c6481d0bb68490fa805a
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2024-01-04 10:10:35 +0000

    maths

---
+++ sub/numbers.js
@@ -0,0 +1,44 @@
+const score = 200
+//  console.log(score);
+
+const balance = new Number(100)
+//  console.log(balance);
+
+//  console.log(balance.toString().length);
+
+//  console.log(balance.toFixed(2));
+   
+
+ const num = 34.6765
+//  console.log(num.toPrecision(3));
+
+
+ const num1 = 345.6765
+//  console.log(num1.toPrecision(3));
+
+ const hundreds = 1000000
+ //console.log(hundreds.toLocaleString('en-IN'));
+
+ //****************maths*******************
+
+//  console.log(Math);
+
+//  console.log(Math.abs(-6));
+
+//  console.log(Math.round(4.4));
+
+//  console.log(Math.ceil(4.2));
+ 
+//  console.log(Math.floor(4.8));
+//  console.log(Math.min(4,3,5,1));
+//  console.log(Math.max(4,8,5,9));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+  
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
