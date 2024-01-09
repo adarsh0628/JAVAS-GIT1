093e89a838ec35fa872befaad0746d7c51019968
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2024-01-09 18:42:22 +0000

    array

---
+++ sub/array.js
@@ -0,0 +1,37 @@
+const MyArr = [0, 1, 2, 3, 4, 5]
+
+const names = ["Ritesh","Nitish","Prerna"]
+
+const  myArray = new Array(1, 2, 3, 4)
+
+console.log(names[0]);
+
+// Array Methods
+//  MyArr.push(6)
+// MyArr.push(7)
+// MyArr.pop()
+
+// MyArr.unshift(9)
+// MyArr.shift()
+
+
+// console.log(MyArr.includes(6));
+// const Newarr = MyArr.join()
+
+// console.log(MyArr);
+// console.log(Newarr);
+
+// slice, splice
+ 
+console.log("A ", MyArr);
+
+const arr = MyArr.slice(1, 3)
+console.log(arr);
+console.log("B ", MyArr);
+
+const arr1 = MyArr.splice(1, 3)
+console.log(arr1);
+console.log("c ", MyArr);
+
+
+
