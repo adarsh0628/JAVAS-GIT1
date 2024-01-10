9c0491d23cb74911e9344454fcc1e4b1464b5209
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2024-01-10 18:25:02 +0000

    obejects

---
+++ sub/objects2.js
@@ -0,0 +1,25 @@
+//  Singleton
+
+// const Nexlerate = Object()
+
+// console.log(Nexlerate);
+
+const user = {}
+
+user.id = "123345"
+user.name = "Chuimui"
+user.IsloggedIn = false
+
+console.log(user);
+
+const user1 = {
+    email: "adarsh@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Gullu",
+            lastname: "Singh"
+        }
+    }
+}
+
+// console.log(user1.fullname.userfullname);
