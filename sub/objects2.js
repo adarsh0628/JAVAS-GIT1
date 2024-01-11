d893e6fef0df81204161d1f4587b16207db74899
adarsh0628 <154824872+adarsh0628@users.noreply.github.com>
2024-01-11 10:57:46 +0000

    json

--- sub/objects2.js
+++ sub/objects2.js
@@ -10,7 +10,7 @@ user.id = "123345"
 user.name = "Chuimui"
 user.IsloggedIn = false
 
-console.log(user);
+// console.log(user);
 
 const user1 = {
     email: "adarsh@gmail.com",
@@ -23,3 +23,51 @@ const user1 = {
 }
 
 // console.log(user1.fullname.userfullname);
+
+const obj1 ={1: "a", 2: "b"}
+const obj2 ={3: "c", 4: "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 ={...obj1, ...obj2}
+
+// console.log(obj3); 
+
+const users =[
+    {
+        id: 1,
+        email: "rt@gmail"
+    },
+    {
+        id: 2,
+        email: "T@gmail"
+    },
+
+]
+
+users[1].email
+// console.log(user);
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// console.log(user.hasOwnProperty('name'));
+
+const fee ={
+    coursename: "c lang",
+    price: "999",
+    courseteacher: "Ritesh"
+}
+
+// fee.coursename
+
+const {courseteacher: teacher} = fee
+
+console.log(teacher);
+
+// {
+//     "name": "Ritesh",
+//     "coursename": "js",
+//     "price": "free"
+// }
